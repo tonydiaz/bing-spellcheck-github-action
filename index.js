721a4533483d72d31a0d3efa8f6d86ea1f6b5f34
Tony Diaz <td@tonydiaz.me>
2020-08-15 19:39:43 -0400

    Added azure packages

--- index.js
+++ index.js
@@ -1,38 +1,63 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const http = require('@actions/http-client');
-
+// const http = require('@actions/http-client');
+const { SpellCheckClient } = require("@azure/cognitiveservices-spellcheck");
+const { CognitiveServicesCredentials } = require("@azure/ms-rest-azure-js");
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
     console.log("Started the index")
     const spellcheckKey = core.getInput("spellchecker-key");
-    const spellcheckEndpoint = "https://api.cognitive.microsoft.com/bing/v7.0/SpellCheck";//core.getInput("spellchecker-endpoint");
+    const spellcheckEndpoint = core.getInput("spellchecker-endpoint");
     const githubSecret = core.getInput("github-secret");
   
     const octokit = new github.GitHub(githubSecret);
     const comment = {body:"Hello worlld"};//github.context.payload.comment;
     console.log("endpoint", spellcheckEndpoint);
 
+    const cognitiveServiceCredentials = new CognitiveServicesCredentials(
+      spellcheckKey
+    );
+    const client = new SpellCheckClient(cognitiveServiceCredentials, {
+      endpoint: spellcheckEndpoint
+    });
+
     if (comment) {
       const text = comment.body;
       console.log('text', text);
-  
+      let newCommentBody;
+
+      client
+      .spellChecker("Bill Gatos", options)
+      .then(result => {
+        console.log("The result is: ");
+        result.flaggedTokens.forEach(flaggedToken => {
+          flaggedToken.suggestions.forEach(suggestion => {
+            console.log(suggestion);
+          });
+        });
+        //Take results and update string
+        //For each update check if the suggestions is greater than 75%
+        //updated the words in the text
+        newCommentBody = "Hello world";
+      })
+      .catch(err => {
+        console.log("An error occurred:");
+        console.error(err);
+      });
+
       //Call Bing API with Text
-      const _http = new http.HttpClient();
-      _http.requestOptions = {
-        headers: {
-          'Ocp-Apim-Subscription-Key': spellcheckKey
-        }
-      }
-      console.log('_http.requestOptions', _http.requestOptions);
-      let response = await _http.post(spellcheckEndpoint, JSON.stringify({text: text}));
-      console.log('response', response);
-      //Take results and update string
-      //For each update check if the suggestions is greater than 75%
-      //updated the words in the text
-      let newCommentBody = "Hello world";
+      // const _http = new http.HttpClient();
+      // _http.requestOptions = {
+      //   headers: {
+      //     'Ocp-Apim-Subscription-Key': spellcheckKey
+      //   }
+      // }
+      // console.log('_http.requestOptions', _http.requestOptions);
+      // let response = await _http.postJson(spellcheckEndpoint, JSON.stringify({text: text}));
+      // console.log('response', response);
+      
       //Update the comment with the corrected spelling
       await octokit.issues.updateComment({
         owner: github.context.actor,
