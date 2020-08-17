c22f51eba93b15e8f8b3cb6cd88194b0895510e4
Tony Diaz <td@tonydiaz.me>
2020-08-17 09:51:51 -0400

    Cleanup code to publishing

--- README.md
+++ README.md
@@ -1,68 +1,22 @@
 # Bing spell check github action
 
-This action uses the Bing spell checker API to correct spelling errors in issue comments. You will need to register for an Azure account to get the key and endpoint to use for this action.
+This action uses the Bing spell checker API to correct spelling errors in issue comments. You will need to register for an Microsoft Azure account to get the Bing spell check key and endpoint to use for this action. 
 
 ## Inputs
+
 ### Required
-    The Bing spell check API key. Given when registering the service in Azure.
-    The Bing spell check endpoint. Given when registering the service in Azure.
-    THe Spell check confidence. How confident should the suggested correction be before update the word. THe default is 80%
+  **SPELLCHECKER_KEY** - The Bing spell check API key. Given when registering the service in Azure.
+  **SPELLCHECKER_ENDPOINT** - The Bing spell check endpoint. Given when registering the service in Azure.
+  **GITHUB_TOKEN** - Auto-created token from github for actions
+  Provide a spell check confidence value. How confident should the suggested correction be to update the word? The default is 80%
 
 ## Example usage
 The key and endpoint should be be added as envrionment variables
 ```yaml
-uses: actions/bing-spellcheck-github-action@master
+uses: actions/bing-spellcheck-github-action@main
 with:
   spellchecker-key: ${{ secrets.SPELLCHECKER_KEY }}
   spellchecker-endpoint: ${{ secrets.SPELLCHECKER_ENDPOINT }}
   spellcheck-confidence: 0.90
+  github-secret: ${{ secrets.GITHUB_TOKEN }}
 ```
-
-## Package for distribution
-
-GitHub Actions will run the entry point from the action.yml. Packaging assembles the code into one file that can be checked in to Git, enabling fast and reliable execution and preventing the need to check in node_modules.
-
-Actions are run from GitHub repos.  Packaging the action will create a packaged action in the dist folder.
-
-Run prepare
-
-```bash
-npm run prepare
-```
-
-Since the packaged index.js is run from the dist folder.
-
-```bash
-git add dist
-```
-
-## Create a release branch
-
-Users shouldn't consume the action from master since that would be latest code and actions can break compatibility between major versions.
-
-Checkin to the v1 release branch
-
-```bash
-git checkout -b v1
-git commit -a -m "v1 release"
-```
-
-```bash
-git push origin v1
-```
-
-Your action is now published! :rocket:
-
-See the [versioning documentation](https://github.com/actions/toolkit/blob/master/docs/action-versioning.md)
-
-## Usage
-
-You can now consume the action by referencing the v1 branch
-
-```yaml
-uses: actions/javascript-action@v1
-with:
-  milliseconds: 1000
-```
-
-See the [actions tab](https://github.com/actions/javascript-action/actions) for runs of this action! :rocket:
--- dist/index.js
+++ dist/index.js
@@ -758,7 +758,6 @@ const { CognitiveServicesCredentials } = __webpack_require__(345);
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    console.log("Started the index")
     const spellcheckKey = core.getInput("spellchecker-key");
     const spellcheckEndpoint = core.getInput("spellchecker-endpoint");
     const spellcheckConfidence = core.getInput("spellchecker-confidence");
@@ -766,8 +765,6 @@ async function run() {
   
     const octokit = new github.GitHub(githubSecret);
     const comment = github.context.payload.comment;
-  
-
     const cognitiveServiceCredentials = new CognitiveServicesCredentials(
       spellcheckKey
     );
@@ -780,35 +777,34 @@ async function run() {
         mode: "proof",
         pragma: "no-cache"
       };
-console.log("call spellcheck");
+
+      if(comment.body.length > 10000) {
+        core.setFailed('The length of the text exceeds the max (10,000) amount of allowed characters');
+        return;
+      }
+console.log(comment.body)
       await client
       .spellChecker(comment.body, options)
       .then(result => {
+        let correctedComment = comment.body;
         result.flaggedTokens.forEach(flaggedToken => {
           if(flaggedToken.suggestions) {
-            console.log("suggestions", flaggedToken.suggestions);
             if(flaggedToken.suggestions[0].score >= spellcheckConfidence ) {
-              let correctedComment = comment.body;
-              
-              console.log("(flaggedToken.token",  flaggedToken.token)
-              console.log("(flaggedToken.suggestions[0].suggestion",  flaggedToken.suggestions[0].suggestion)
-              correctedComment = comment.body.replace(flaggedToken.token, flaggedToken.suggestions[0].suggestion);
-              console.log("update the comment",  correctedComment)
-              //Update the comment with the corrected spelling
-               octokit.issues.updateComment({
-                owner: github.context.actor,
-                repo: github.context.payload.repository.name,
-                comment_id: comment.id,
-                body: correctedComment
-              });
-              console.log("comment updated");
+              correctedComment = comment.body.replace(flaggedToken.token, flaggedToken.suggestions[0].suggestion)
             }
           }
         });
+        //Update the comment with the corrected spelling
+         octokit.issues.updateComment({
+          owner: github.context.actor,
+          repo: github.context.payload.repository.name,
+          comment_id: comment.id,
+          body: correctedComment
+        });
       })
       .catch(err => {
-        console.log("An error occurred:");
-        console.error(err);
+        console.log("An error occurred:", err);
+        core.setFailed(err);
       });
 
 
